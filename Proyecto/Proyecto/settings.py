cde48c5958a33d38b4a2b915f8cf797b1a3eb2c3
Kanade <celestialwarrior90000@gmail.com>
2024-06-20 16:19:05 -0400

    creacion de la aplicacion, y referencia de la misma en el archivo settings

--- Proyecto/Proyecto/settings.py
+++ Proyecto/Proyecto/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    ##Nuestras apps aqui abajo
+    'APP.apps.AppConfig'
 ]
 
 MIDDLEWARE = [
