64936c3cce27d080868ce36e12e6249c531139d8
Kanade <celestialwarrior90000@gmail.com>
2024-06-20 01:24:06 -0400

    Se cambio el motor de la bd a mysql y se creo el archivo requirements.txt...MORTADELA XD

--- Proyecto/Proyecto/settings.py
+++ Proyecto/Proyecto/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'Proyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'WueQpoIUisouHKUFsEbIJbcAZbfAjhis',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '13619'
+
     }
 }
 
